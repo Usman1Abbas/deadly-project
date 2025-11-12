05ca98f2c66bc1b62a84e894115bd9e645923dee
Muhammad Usman <usmanabbas500030@gmail.com>
2025-11-12 20:21:16 +0500

    Initial commit: Add db/seeds/initial_data.sql

---
+++ db/seeds/initial_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Products (Name, Description, Price) VALUES
+('Product 1', 'Description for Product 1', 19.99),
+('Product 2', 'Description for Product 2', 29.99),
+('Product 3', 'Description for Product 3', 39.99);
