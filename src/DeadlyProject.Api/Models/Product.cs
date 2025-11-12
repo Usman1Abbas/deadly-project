d9842171c1227f83ce3f257b250d2c7c8ef12756
Muhammad Usman <usmanabbas500030@gmail.com>
2025-11-12 20:20:30 +0500

    Initial commit: Add src/DeadlyProject.Api/Models/Product.cs

---
+++ src/DeadlyProject.Api/Models/Product.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DeadlyProject.Api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+        public string? Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters")]
+        public string? Description { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+    }
+}
